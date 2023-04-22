3da2b6b445bf11e1e2c89897b7f52c96bc2cc756
MoisesGHT <MoisesGHT0923@gmail.com>
2023-04-21 20:44:28 -0500

    tercer commit

--- index2.php
+++ index2.php
@@ -50,3 +50,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Example</title>
+</head>
+<body>
+    <h1>People</h1>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Actions</th>
+        </tr>
+        <?php foreach ($people as $person): ?>
+            <tr>
+                <td><?php echo $person["id"]; ?></td>
+                <td><?php echo $person["name"]; ?></td>
+                <td><?php echo $person["age"]; ?></td>
+                <td>
+                    <form method="post" action="">
+                      <input type="hidden" name="id" value="<?php echo $person["id"];?>">
+                      <input type="hidden" name="action" value="update">
+                      <input type="hidden" name="name" value="<?php echo $person["name"];?>">
+                      <input type="hidden" name="age" value="<?php echo $person["age"];?>">
+                      <button type="submit">Update</button>
+                    </form>
+                    <form method="post" action="">
+                        <input type="hidden" name="id" value="<?php echo $person["id"]; ?>">
+                        <input type="hidden" name="action" value="delete">
+                        <button type="submit">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+        <h2>Create Person</h2>
+        <form method="post" action="">
+            <input type="hidden" name="action" value="create">
+            <input type="text" name="name" placeholder="Name">
+            <input type="text" name="age" placeholder="Age">
+            <button type="submit">Create</button>
+        </form>
+</body>
+</html>
